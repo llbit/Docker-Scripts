22fcbd638415792357490e8d615f38f7efe6755c
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2018-04-06 16:34:12 +0200

    Add start script

---
+++ experiment/start-networked.sh
@@ -0,0 +1,5 @@
+# Starts a temporary container for experimenting with network access.
+docker run -it \
+  --rm \
+  --name scratch \
+  experiment
--- experiment/start.sh
+++ experiment/start.sh
@@ -1,11 +1,7 @@
+# Starts a temporary container for experimenting without network access.
 docker build -t experiment .
 docker run -it \
   --rm \
   --network none \
   --name scratch \
   experiment
-
-#docker run -it \
-#  --rm \
-#  --name scratch \
-#  experiment
