e29804083af776bfa16c5942868b0ff1b2a395f2
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2018-04-23 10:47:59 +0200

    Add Jenkins notes

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -1,3 +1,7 @@
+# Note: to create docker volume inside nfs mount:
+# docker volume create --opt type=none --opt o=bind --opt device=/mnt/vmstore/jenkins_home jenkins_home
+# NB: /mnt/vmstore/jenkins_home should be created first.
+
 # Note: update Jenkins by running docker pull jenkins/jenkins:lts
 docker run \
   --name jenkins \
