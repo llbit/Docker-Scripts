a7056f3162463760e55c4034d276a99866e466e4
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2019-10-28 13:18:41 +0100

    Update jenkins-gcc

--- jenkins_gcc/jenkins_start.sh
+++ jenkins_gcc/jenkins_start.sh
@@ -4,6 +4,7 @@ docker build -t jenkins_gcc .
 docker run \
 	--name jenkins \
 	--detach \
-	-p 8080:8080 -p 50000:50000 \
-	-v jenkins_home:/var/jenkins_home \
+	-u $(id -u):$(id -g) \
+	-p 80:8080 -p 50000:50000 \
+	-v /jasttest/jenkins_home:/var/jenkins_home \
 	jenkins_gcc
